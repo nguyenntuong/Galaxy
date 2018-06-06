4bff419b8ed53b7fe381e3c14a8fd1aa781094fe
Tường Nguyễn <nguyenntuong@outlook.com.vn>
2018-06-06 11:45:06 +0700

    Chua lam gi het

--- Galaxy/main.cpp
+++ Galaxy/main.cpp
@@ -1,9 +1,56 @@
 #include<iostream>
+#include<list>
+#include<SFML\Graphics.hpp>
+
+using namespace sf;
+
+Sprite backgroundFrame;
+Texture backgroundTexture;
+
+std::list<Sprite> assetFrame;
+std::list<Texture> assetTexture;
+
+std::list<Sprite> shipFrame;
+std::list<Texture> shipTexture;
+
+std::list<Sprite> enemyFrame;
+std::list<Texture> enemyTexture;
+
+std::list<Sprite> friendFrame;
+std::list<Texture> friendTexture;
+
+std::list<Sprite> itemFrame;
+std::list<Texture> itemTexture;
+
+std::list<Sprite> controlFrame;
+std::list<Texture> controlTexture;
+
+std::list<Sprite> bulletFrame;
+std::list<Texture> bulletTexture;
 
 
 int main()
 {
-	printf_s("New World!");
-	getchar();
+	RenderWindow gw(VideoMode(960, 540), "Galaxy");
+	gw.setFramerateLimit(60);
+
+	while (gw.isOpen())
+	{
+		Event ge;
+
+		while (gw.pollEvent(ge))
+		{
+			if (ge.type == Event::Closed)
+			{
+				gw.close();
+			}
+		}
+
+		//// Draw ////
+		gw.clear();
+		gw.draw();
+		gw.display();
+	}
+	
 	return 0;
 }
